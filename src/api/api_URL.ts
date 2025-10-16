b784189cb21cc525b3f924f61c6eab86462fc748
chonuboy <knanthakumar12@gmail.com>
2025-10-16 10:01:33 +0530

    Release v-1.2

--- src/api/api_URL.ts
+++ src/api/api_URL.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:9090/";
+// export const API_URL = "http://localhost:9090/";
 // export const swaggerurl = "http://fee7-110-235-232-108.ngrok-free.app"
-// export const API_URL = "http://35.200.234.26:9090/";
+export const API_URL = "http://35.200.234.26:9090/";
